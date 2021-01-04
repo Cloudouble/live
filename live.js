a95c8e9250d2a768809d4acf542998eeab179d36
David Pitman <david.pitman@cloudouble.com>
2021-01-04 13:29:33 +0000

    Initial Commit

--- live.js
+++ live.js
@@ -3,22 +3,42 @@ window.LiveElement.Live = window.LiveElement.Live || Object.defineProperties({},
     version: {configurable: false, enumerable: true, writable: false, value: '1.0.0'}, 
     loopMaxMs: {configurable: false, enumerable: true, writable: false, value: 1000}, 
     defaultListenerDelay: {configurable: false, enumerable: true, writable: false, value: 1000}, 
-    subscriptions: {configurable: false, enumerable: true, writable: false, value: {}}, 
     listeners: {configurable: false, enumerable: true, writable: false, value: {}}, 
     processors: {configurable: false, enumerable: true, writable: false, value: {}}, 
-    processListener: {configurable: false, enumerable: false, writable: false, value: function(key, config) {
-        //console.log('line 10', key)
+    runListener: {configurable: false, enumerable: false, writable: false, value: function(key, config) {
         var now = Date.now()
         if (config && typeof config == 'object'
+            && !config.expired 
             && typeof config.processor == 'string' && typeof window.LiveElement.Live.processors[config.processor] == 'function'
             && (((config.last || 0) + (config.delay || window.LiveElement.Live.defaultListenerDelay)) < now)) {
-            window.LiveElement.Live.listeners[key].last = now
-            window.LiveElement.Live.processors[config.processor]()
+            if (config.expires && (config.expires <= now)) {
+                config.expired = true
+                window.dispatchEvent(new window.CustomEvent('live-listener-expired', {detail: {listener: key, config: config}}))
+                window.dispatchEvent(new window.CustomEvent(`live-listener-expired-${key}`, {detail: {listener: key, config: config}}))
+            } else if (config.count && config.max && (config.count >= config.max)) {
+                config.expired = true
+                window.dispatchEvent(new window.CustomEvent('live-listener-maxed', {detail: {listener: key, config: config}}))
+                window.dispatchEvent(new window.CustomEvent(`live-listener-maxed-${key}`, {detail: {listener: key, config: config}}))
+            } else if (config.next && (config.next > now)) {
+                window.dispatchEvent(new window.CustomEvent('live-listener-passed', {detail: {listener: key, config: config}}))
+                window.dispatchEvent(new window.CustomEvent(`live-listener-passed-${key}`, {detail: {listener: key, config: config}}))
+            } else {
+                config.last = now
+                config.count = (config.count || 0) + 1
+                var showconfig = {...config}
+                if (config.next) {
+                    showconfig.next = config.next
+                    delete config.next
+                }
+                var payload = window.LiveElement.Live.processors[config.processor]()
+                window.dispatchEvent(new window.CustomEvent('live-listener-run', {detail: {listener: key, config: showconfig, payload: payload}}))
+                window.dispatchEvent(new window.CustomEvent(`live-listener-run-${key}`, {detail: {listener: key, config: showconfig, payload: payload}}))
+            }
         }
     }},
     run: {configurable: false, enumerable: false, writable: false, value: function() {
         Object.entries(window.LiveElement.Live.listeners).forEach(entry => {
-            window.LiveElement.Live.processListener(...entry)
+            window.LiveElement.Live.runListener(...entry)
         })
         /*document.querySelectorAll('[live-subscription]').forEach(subscribedElement => {
             
