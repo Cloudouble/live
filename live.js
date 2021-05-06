fb990be99408fec9e0ad85158515e9dc83da6491
David Pitman <david.pitman@cloudouble.com>
2021-05-06 08:24:33 +0000

    changes for 1.0.5

--- live.js
+++ live.js
@@ -1,6 +1,6 @@
 window.LiveElement = window.LiveElement || {}
 window.LiveElement.Live = window.LiveElement.Live || Object.defineProperties({}, {
-    version: {configurable: false, enumerable: true, writable: false, value: '1.0.4'}, 
+    version: {configurable: false, enumerable: true, writable: false, value: '1.0.5'}, 
     loopMaxMs: {configurable: false, enumerable: true, writable: false, value: 1000}, 
     defaultListenerDelay: {configurable: false, enumerable: true, writable: false, value: 1000}, 
     listeners: {configurable: false, enumerable: true, writable: false, value: {}}, 
@@ -153,6 +153,7 @@ window.LiveElement.Live = window.LiveElement.Live || Object.defineProperties({},
                                             ...{'#value': element.value, '#innerHTML': element.innerHTML, '#innerText': element.innerText}
                                         }, 
                                         triggersource: element, 
+                                        event: event,
                                         vector: vector
                                     }
                                 }
